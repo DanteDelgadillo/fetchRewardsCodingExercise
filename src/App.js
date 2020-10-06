8abd70c4d41058f5acf0d051cac6e812b55e2362
dante <40077234+DanteDelgadillo@users.noreply.github.com>
2020-10-06 12:14:44 -0700

    full push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from "axios"
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([])
+
+
+  // Fetching data from https://fetch-hiring.s3.amazonaws.com/hiring.json
+  useEffect(() => {
+    async function fetchData() {
+      // ****** ened up using  https://cors-anywhere.herokuapp.com to bypass cors *******
+      const request = await axios.get("https://cors-anywhere.herokuapp.com/https://fetch-hiring.s3.amazonaws.com/hiring.json");
+      setData(request.data)
+      return request.data
+    }
+    fetchData()
+  }, [])
+
+  // ************* filted data here ***********
+  const newArray = data.filter(item => item.name !== "");
+  const secondArray = newArray.filter(items => items.name !== null);
+  const sortArray = secondArray.sort(function (a, b) { return a.listId - b.listId });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="tableDiv">
+      <table className="table">
+        <tbody>
+          <tr>
+            <th>ListId</th>
+            <th>Name</th>
+          </tr>
+          {/* map data */}
+          {sortArray.map((item, i) => (
+            <tr key={i}>
+              <td>{item.listId}</td>
+              <td>{item.name}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
